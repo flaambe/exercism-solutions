f9938966fe5fa9d5bd97c82ec3f9bda15a05ef81
Grigoriy <flaambe@gmail.com>
2021-06-01 20:43:41 +0500

    go: improve leap solution

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,13 +3,9 @@ package leap
 
 // IsLeapYear returns true if the given year is a leap year.
 func IsLeapYear(year int) bool {
-	if year%4 != 0 {
-		return false
-	} else if year%100 != 0 {
-		return true
-	} else if year%400 != 0 {
-		return false
+	if year%100 == 0 {
+		return year%400 == 0
 	}
 
-	return true
+	return year%4 == 0
 }
